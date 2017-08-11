66e62f1ed319e08cc701a4e96568b89022d265ec
Arian Kordi <ariankordi@ariankordi.net>
2017-08-11 01:13:25 -0400

    I forgot something

--- closedverse_main/models.py
+++ closedverse_main/models.py
@@ -36,7 +36,7 @@ class UserManager(BaseUserManager):
 			username=username,
 			password=password,
 		)
-		user.is_staff = True
+		user.staff = True
 		user.save(using=self._db)
 		return user
 	def authenticate(self, username, password):
